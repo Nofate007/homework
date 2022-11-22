1ac67b63902d40f0d1898aac6502ccb5bfd4f64e
Timofey <shashura.t@yandex.ru>
2022-11-22 17:59:53 +0300

    task 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -5,4 +5,37 @@
 782 -> 8
 918 -> 1
 */
-int 
+
+
+// Создадим функция получения случайного числа в отрезке
+int GetRandomNumberInRange (int start, int end)         // start end - задаем граничные параметры для отрезка (входный знвчения)
+{
+    int resulut = 0;                                    //  вводим переменную result равную 0
+    resulut = new Random().Next(start, end + 1);        //  присваиваем ее значение из отрезка (+1 для того чтобы включить последнее(последнее не включается))
+    return resulut;                                     //  выводим значение (выходные знвчения)
+}
+
+
+// Создадим функция получения второй цифры числа
+
+int GetDigitTwo(int number)                                // (входный знвчения)
+{
+    int digitProm = number / 10;                           // выдаст вторую и третью цифру трехзначного числа (слева)
+//    Console.WriteLine(digitProm);                          // выводим и смотрим вторую и третью цифру трехзначного числа (слева)
+    int digitTwo = digitProm %10;                          // выдаст вторую и цифру трехзначного числа (слева)
+    return digitTwo;                                       //  выводим значение второй цифры (выходные знвчения)
+}                          
+
+
+
+
+int randomNumber = GetRandomNumberInRange(100,1000);
+Console.WriteLine("случайное трхзначное число");
+Console.WriteLine(randomNumber);
+
+int outdata = GetDigitTwo(randomNumber);
+Console.WriteLine("вторая цифра трхзначного числа");
+Console.WriteLine(outdata);
+
+
+
