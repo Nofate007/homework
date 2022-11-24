060b590317137eaa18c559e2777698989a5456df
Timofey <shashura.t@yandex.ru>
2022-11-24 17:32:38 +0300

    task 23

---
+++ task23/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+*/
+
+
+int getDataFromUser(string textdata)
+{
+int outData = 0;
+Console.WriteLine(textdata);
+outData = Convert.ToInt32(Console.ReadLine());
+return outData;
+}
+
+
+
+int tablitsaKubov (int inputData)
+{
+    for (int i=1; i<inputData+1; i++)
+    {
+        double kub = Math.Pow(i, 3);                //возведение числа в степень 
+        Console.Write($"{kub} "); 
+    }
+return inputData;
+}
+
+
+
+Console.Write("Введите число N"); 
+int number = getDataFromUser(" ");
+Console.WriteLine("кубы чисел от 1 до N ");
+double kub = tablitsaKubov(number);
